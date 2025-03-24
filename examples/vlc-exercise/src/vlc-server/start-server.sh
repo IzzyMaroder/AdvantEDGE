9f0e8d4a1c7a36482c900f3b168b7471f7ed4059
IzzyMaroder <leonardo.spinelli2001@gmail.com>
2025-03-24 18:34:03 +0000

    modified server

--- examples/vlc-exercise/src/vlc-server/start-server.sh
+++ examples/vlc-exercise/src/vlc-server/start-server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ffmpeg -re -i video10m.mp4 -c:v h264 -b:v 300k -preset ultrafast -c:a aac -b:a 128k -ac 2 -ar 44100 -f flv tcp://0.0.0.0:9090?listen
+ffmpeg -re -i video10m.mp4 -c:v h264 -b:v 300k -preset ultrafast -c:a aac -b:a 128k -ac 2 -ar 44100 -f hls -hls_time 10 -hls_list_size 6 -hls_segment_filename "segment_%03d.ts" playlist.m3u8 &
+
+python3 -m http.server 9090
