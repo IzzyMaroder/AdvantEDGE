070f3a446a5e801c84ef22cf3b82af7538130522
leonardo spinelli <leonardo.spinelli@edu.unito.it>
2025-06-19 19:38:51 +0200

    modifed ffmepg command

--- examples/vlc-exercise/src/vlc-server/start-server.sh
+++ examples/vlc-exercise/src/vlc-server/start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ffmpeg -re -i videocartoon.mp4 -c:v h264 -b:v 300k -preset ultrafast -c:a aac -b:a 128k -ac 2 -ar 44100 -f hls -hls_time 10 -hls_list_size 6 -hls_segment_filename "segment_%03d.ts" playlist.m3u8 &
+ffmpeg -re -i videocartoon.mp4 -c:v h264 -b:v 1200k -preset veryfast -c:a aac -b:a 128k -ac 2 -ar 44100 -f hls -hls_time 10 -hls_list_size 6 -hls_segment_filename "segment_%03d.ts" playlist.m3u8 &
 
 python3 -m http.server 9090
